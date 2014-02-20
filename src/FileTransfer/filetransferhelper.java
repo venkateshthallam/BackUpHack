812568274554e111d3d44a81e6e27d9760081c19
venkateshthallam <venkateshthallam@gmail.com>
2014-02-19 22:52:15 -0500

    Added the directory copy and zipping functionality

--- src/FileTransfer/filetransferhelper.java
+++ src/FileTransfer/filetransferhelper.java
@@ -4,6 +4,14 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
@@ -14,6 +22,11 @@ import java.nio.channels.FileLock;
 import java.nio.channels.OverlappingFileLockException;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipOutputStream;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -22,121 +35,124 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.io.FileUtils;
 
- public class filetransferhelper extends javax.servlet.http.HttpServlet implements javax.servlet.Servlet {
-   static final long serialVersionUID = 1L;
-   
-    /* (non-Java-doc)
+public class filetransferhelper extends javax.servlet.http.HttpServlet
+		implements javax.servlet.Servlet {
+	static final long serialVersionUID = 1L;
+	List<String> fileList;
+	
+	/* (non-Java-doc)
 	 * @see javax.servlet.http.HttpServlet#HttpServlet()
 	 */
 	public filetransferhelper() {
 		super();
-	}  
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher rd=request.getRequestDispatcher("copied.jsp");
-		PrintWriter out=response.getWriter();
-		String inputPath=request.getParameter("ip");
-		String outputPath=request.getParameter("op");
-		if(inputPath!=null && outputPath!=null)
-		{
-			StandardCopyOption[] opts =
-			{StandardCopyOption.REPLACE_EXISTING,
-			StandardCopyOption.COPY_ATTRIBUTES};
-			
-			/*FileChannel inputChannel = null;
-		    FileChannel outputChannel = null;*/
-		   	File sourceFile=new File(inputPath);
-			File destFile=new File(outputPath);
-			if(sourceFile.isDirectory())
-			{
-				//copyFolder(sourceFile,destFile);
-				copyDirectory(sourceFile,destFile);
-			}
-			else
-			{
-			    try
-			    {
-			    	Files.copy(sourceFile.toPath(), destFile.toPath(),opts);
-			    	} finally 
-			    		{
-			    		/*if(inputChannel!=null && outputChannel!=null){
-				        inputChannel.close();
-				        outputChannel.close();
-			    		}*/
-					  }
-			    	rd.forward(request,response);
-				}
-			}
-		else
-			out.write("Please enter proper input and output paths");
-	}   	  	    
-	public static void copyFolder(File src, File dest)
-	throws IOException{
-
-	if(src.isDirectory()){
-
-		//if directory not exists, create it
-		if(!dest.exists()){
-		   dest.mkdir();
-		   System.out.println("Directory copied from " 
-                          + src + "  to " + dest);
-		}
-		
-		//list all the directory contents
-		String files[] = src.list();
-
-		for (String file : files) {
-		   //construct the src and dest file structure
-		   File srcFile = new File(src, file);
-		   File destFile = new File(dest, file);
-		   //recursive copy
-		   copyFolder(srcFile,destFile);
-		}
-
-	}else{
-		//if file, then copy it
-		//Use bytes stream to support all file types
-		InputStream in = new FileInputStream(src);
-	        OutputStream out = new FileOutputStream(dest); 
-
-	        byte[] buffer = new byte[1024];
-
-	        int length;
-	        //copy the file content in bytes 
-	        while ((length = in.read(buffer)) > 0){
-	    	   out.write(buffer, 0, length);
-	        }
-
-	        in.close();
-	        out.close();
-	        System.out.println("File copied from " + src + " to " + dest);
 	}
-}
-	public void copyDirectory(File sourceLocation , File targetLocation) throws IOException {
-		FileLock lock=null;
-		FileChannel channel=null;
-		try{
-	    if (sourceLocation.isDirectory()) {
-	        if (!targetLocation.exists()) {
-	            targetLocation.mkdir();
-	        }
 
-	      /*  String[] children = sourceLocation.list();
-	        for (int i=0; i<children.length; i++) {
-	            copyDirectory(new File(sourceLocation, children[i]),
-	                    new File(targetLocation, children[i]));
-	        }*/
-	        try {
-	            FileUtils.copyDirectory(sourceLocation, targetLocation);
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	        }
-	        
-	    } 
-		}
-		finally{
-				/*lock.close();
-				channel.close();*/
-				
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher("copied.jsp");
+		PrintWriter out = response.getWriter();
+		String inputPath = request.getParameter("ip");
+		String outputPath = request.getParameter("op");
+		String compressFlag=request.getParameter("Compress");
+		out.print(compressFlag);
+		fileList = new ArrayList<String>();
+		if (inputPath != null && outputPath != null) {
+			File sourceFile = new File(inputPath);
+			File destFile = new File(outputPath);
+			if(compressFlag.equalsIgnoreCase("Compress")){
+			      generateFileList(sourceFile);
+			      zipIt(outputPath,inputPath);
+			}
+			if (sourceFile.isDirectory()) {				
+				copyDir(sourceFile,destFile);
+			} else {
+				copyFile(sourceFile, destFile);
+				rd.forward(request, response);
+
+			}
 		}
 	}
+
+	public static void copyFile(File sourceFile, File destFile)
+			throws IOException {
+		StandardCopyOption[] opts = { StandardCopyOption.REPLACE_EXISTING,
+				StandardCopyOption.COPY_ATTRIBUTES };
+		Files.copy(sourceFile.toPath(), destFile.toPath(), opts);
+	}
+	
+	public static void copyDir(File sourceLocation, File targetLocatio){
+		try {
+		    FileUtils.copyDirectory(sourceLocation, targetLocatio);
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
+	}
+	 public void zipIt(String zipFile,String SOURCE_FOLDER)
+	  {
+	    byte[] buffer = new byte[1024];
+	    String source="";
+	   try{
+	       try
+	       {
+	           source=SOURCE_FOLDER.substring(SOURCE_FOLDER.lastIndexOf("\\")+1,SOURCE_FOLDER.length());
+	       }
+	       catch(Exception e)
+	       {
+	               source=SOURCE_FOLDER;
+	       }
+	      FileOutputStream fos = new FileOutputStream(zipFile);
+	      ZipOutputStream zos = new ZipOutputStream(fos);
+
+	      System.out.println("Output to Zip : " + zipFile);
+
+	      for(String file : this.fileList){
+
+	          System.out.println("File Added : " + file);
+	          ZipEntry ze= new ZipEntry(source+File.separator+file);
+	          zos.putNextEntry(ze);
+
+	          FileInputStream in =
+	                     new FileInputStream(SOURCE_FOLDER + File.separator + file);
+
+	          int len;
+	          while ((len = in.read(buffer)) > 0) {
+	              zos.write(buffer, 0, len);
+	          }
+
+	          in.close();
+	      }
+
+	      zos.closeEntry();
+	      //remember close it
+	      zos.close();
+
+	      System.out.println("Folder successfully compressed");
+	  }catch(IOException ex){
+	     ex.printStackTrace();
+	  }
+	 }
+	  public void generateFileList(File node,String src){
+
+	      //add file only
+	  if(node.isFile())
+	      {
+	      fileList.add(generateZipEntry(node.toString()),src);
+
+	  }
+
+	  if(node.isDirectory())
+	        {
+	      String[] subNote = node.list();
+	      for(String filename : subNote){
+	          generateFileList(new File(node, filename));
+	         }
+	  }
+
+	  }
+	  private String generateZipEntry(String file,String SOURCE_FOLDER)
+	 {
+	      return file.substring(SOURCE_FOLDER.length()+1, file.length());
+	  }
+
+	
 }
